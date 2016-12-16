e044849ec12a8217d17c91877f2409690188c15f
Charles Berry <ccberry@ucsd.edu>
2016-12-16 15:47:26 -0800

    annotate for inclusion in latex doc

--- src/innerLoop.c
+++ src/innerLoop.c
@@ -16,6 +16,7 @@ static void matmult(double *x, int nrx, int ncx,
 		  x, &nrx, y, &nry, &zero, z, &nrx);
 }
 
+// //* As per Equation \ref{eq:prz}
 void probzv( double *V, double *Z, int *T){
   double usable=1.0;
   for (int t = 0; t < *T; t++) {
@@ -50,6 +51,8 @@ void zysum(
     double biglog=R_NegInf;
     for (int t=0;t<*T;t++){
       int one=1L;
+      // //* As per Equation \ref{eq:prwz3}. 
+      // Note: eps==0 is limiting case 
       double x = (eps[0]==0) ? 0.0 :
 	dnbinom(wp[t],eps[0],eps[1]/(eps[1]+prw[t]),one);
       for (int k=0;k<*ko;k++) x+= log(etaomdp[t+k**T]/prw[t]) *
@@ -62,6 +65,7 @@ void zysum(
       workT[t]=exp(workT[t]-biglog)*pz[t];
       prTot+=workT[t];
     }
+    // //* As per multinomial part of \ref{eq:prw.z}
     for (int t=0;t<*T;t++) workT[t]/=prTot;
     rmultinom((int) n[idat],workT,(int) T[0], zy+idat**T);
   }  
@@ -69,34 +73,35 @@ void zysum(
   PutRNGstate();
 #endif
 }
+/* main entry point                    */
+/* run reps iterations of the sampler */
 
-/* run nthin iterations of the sampler */
-
-void innerLoop(int *reps,
-	       int *T,
-	       int *ka,
-	       int *ko,
-	       int *n,
-	       int *ndat,
-	       int *w,
-	       int *wp,
-	       double * s,
-	       double * lamb,
-	       double *omcp,
-	       double *omdp,
-	       double *eps,
-	       /* modifiable */
-	       double * V,
-	       double * alpha,
-	       double * eta,
-	       int *zy,
-	       double *eoy,
-	       double *etaomdp,
-	       double *prw,
-	       double *pz,
-	       double *workT,
-	       int *xstmp,
-	       int *xsums
+void innerLoop(
+	       int *reps,         // nthin iterations
+	       int *T,            // number of compositions
+	       int *ka,           // number of actual cell types
+	       int *ko,           // number of observed cell types
+	       int *n,            // repetitions of a data pattern
+	       int *ndat,         // number of data patterns
+	       int *w,            // ndat by ko table of counts
+	       int *wp,           // ndat vector of number of cells
+	       double *s,         // prior for alpha
+	       double *lamb,      // prior for row of eta
+	       double *omcp,      // omega times 1-psi
+	       double *omdp,      // omega times diag(psi)
+	       double *eps,       // prior for R
+	       /* modifiable */  
+	       double *V,         // stick lengths
+	       double *alpha,     // prior for V
+	       double *eta,       // compositions
+	       int *zy,           // T by ndat table of assignments
+	       double *eoy,       // workspace
+	       double *etaomdp,   // eta times omdp
+	       double *prw,       // prob of observed count
+	       double *pz,        // prob Z = t
+	       double *workT,     // workspace
+	       int *xstmp,        // workspace 
+	       int *xsums         // T by ka table of counts
 	       ){
 
   /* inits */
@@ -125,6 +130,7 @@ void innerLoop(int *reps,
 
     for (int t=0;t<*T;t++){
       double seen=0.0;
+      // //* See discussion of \ref{eq:xobs} 
       for (int k=0;k<*ka;k++) xsums[t+k**T]=0;
       for (int k=0;k<*ko;k++) {
         /* yz is number of cells in cluster t, cell type k */
@@ -143,9 +149,11 @@ void innerLoop(int *reps,
         rmultinom( (int) yz, eoy, *ka, xstmp);
         for (int k2=0;k2<*ka;k2++) xsums[t+k2**T]+=xstmp[k2];
       }
+      // //* Starting at \ref{eq:unseen}, see discussion
       for (int idat=0;idat<*ndat;idat++)
         seen+= (double) zy[t+idat**T] * (eps[0] + (double) wp[idat]);
       double nbparm = fmax2( 0.0, (1.0-prw[t])/(1.0+eps[1]) );
+      /* R uses 1-nbparm in rnbinom where others might use nbparm */
       int notseen = (seen==0.0)? 0L: (int) rnbinom(seen,1.0-nbparm);
       /* Rprintf("seen = %f notseen = %d\n", seen, notseen); */
       if (notseen!=0){
@@ -167,7 +175,7 @@ void innerLoop(int *reps,
     /* eta */
 
     /* sample eta */
-    
+    // //* As per \ref{eq:eta.xl}
     for (int t=0; t<*T; t++){
       double x=0.0;
       for (int k=0;k<*ka;k++){
@@ -183,8 +191,7 @@ void innerLoop(int *reps,
     
     /* V */
 
-    /* double *V , *workT, *alpha, int *ndat, *T, *zy */
-    
+    // //* See Equations \ref{eq:V.Za}
     V[*T-1L]=1.0;
     for (int t=0;t<*T;t++){
       int x=0.0;
@@ -201,7 +208,7 @@ void innerLoop(int *reps,
 
     /* alpha */
 
-    /* double *alpha, *V, *s, *T */
+    // //* As per \ref{eq:alpha.r}
     double sumclog=0.0;
     for (int t=0; t<*T-1; t++) sumclog+=log(1.0-V[t]);
     alpha[0]=rgamma(s[0]+*T-1,1.0/(s[1]-sumclog));
