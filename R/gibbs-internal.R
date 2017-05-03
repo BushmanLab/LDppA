ce8be8cf07a085e7d47d1e2aa3de10efabd8d78e
Charles Berry <ccberry@ucsd.edu>
2017-05-03 15:53:08 -0700

    logP.Y.eta.V() corrected, added epsilon arg

--- R/gibbs-internal.R
+++ R/gibbs-internal.R
@@ -14,16 +14,21 @@ logP.alpha <- function(alpha,s){
     dgamma(alpha,s[1],rate=s[2],log=TRUE)
 }
 
-logP.Y.eta.V <- function(y,eta,V,omega,n=1,psi=NULL){
+logP.Y.eta.V <- function(y,eta,V,omega,n=1,psi=NULL,epsilon=c(0,0)){
     prob <- exp(log.prob.v(V[-length(V)]))
     pr.y.eta.t <- eta%*%omega
+    pr.seen <- rowSums(pr.y.eta.t)
     if (!is.null(psi)){
         pr.y.eta.t <- sweep(pr.y.eta.t,2,psi,"*")
-        pr.obs <- 1/rowSums(pr.y.eta.t)
-        llk <-
-            (pr.obs * dmulti(y,quick.prop.table(pr.y.eta.t,1)))%*%prob
-    } else {
-        llk <- dmulti(y,quick.prop.table(pr.y.eta.t,1))%*%prob
     }
+    pr.multi <- dmulti(y,quick.prop.table(pr.y.eta.t,1))
+    if (sum(epsilon)>0){
+        pr.wp <-
+            outer(rowSums(y),pr.seen,
+                  function(x,y) dnbinom(x,epsilon[1],
+                                        1-pr.seen/(pr.seen+epsilon[2])))
+        pr.multi <- pr.multi*pr.wp
+    }
+    llk <- pr.multi %*% prob
     sum(n*log(llk))
 }
